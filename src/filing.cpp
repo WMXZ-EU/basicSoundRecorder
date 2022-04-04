3ff2fd5aad50dcd8c29580a05a9247f079a28cc3
WMXZ-EU <WMXZ-EU@users.noreply.github.com>
2022-04-04 19:55:15 +0200

    Update filing.cpp

--- src/filing.cpp
+++ src/filing.cpp
@@ -222,11 +222,16 @@ void sdCsWrite(SdCsPin_t pin, bool level) { digitalWriteFast(pin, level); }
     #define SD_SCK  14
 #endif
 
+// adjust to HW configuration
+// only build-in SD card T3.5/6 and T4.0/1
+    const char *sd_str[]={"sdio"};
+    const int cs[] = {BUILTIN_SDCARD};
+// only audio board
+//    const char *sd_str[]={"sd1"};
+//    const int cs[] = {10 };
+// general example
 //    const char *sd_str[]={"sdio",   "sd1","sd2","sd3","sd4","sd5","sd6"};
 //    const int cs[] = {BUILTIN_SDCARD,  34,   33,   35,   36,   37,  38 };
-
-    const char *sd_str[]={"sd1"};
-    const int cs[] = {10 };
 
     const int nsd = sizeof(cs)/sizeof(int);
 
